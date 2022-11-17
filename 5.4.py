8645ff7cf4148a8bb89adf55f1dd70ba2dea3989
Ruslan Ergashev <rergashev@gmail.com>
2022-11-17 17:31:42 +0500

    Initial commit

--- 5.4.py
+++ 5.4.py
@@ -173,32 +173,32 @@
 # def from_db():
 #     print("some data from database")
 #
-
-from_db()
-
-
-USERS = ['admin', 'guest', 'director', 'root', 'superstar']
-
-yesno = input("""Введите Y, если хотите авторизоваться, или N, 
-             если хотите продолжить работу как анонимный пользователь: """)
-
-auth = yesno == "Y"
-
-if auth:
-    username = input("Введите ваш username:")
-
-@is_auth
-@has_access
-def from_db():
-    print("some data from database")
-
-from_db()
-
-def has_access(func):
-    def wrapper():
-        if username in USERS:
-            print("Авторизован как", username)
-            func()
-        else:
-            print("Доступ пользователю", username, "запрещён")
-    return wrapper
+#
+# from_db()
+#
+#
+# USERS = ['admin', 'guest', 'director', 'root', 'superstar']
+#
+# yesno = input("""Введите Y, если хотите авторизоваться, или N,
+#              если хотите продолжить работу как анонимный пользователь: """)
+#
+# auth = yesno == "Y"
+#
+# if auth:
+#     username = input("Введите ваш username:")
+#
+# @is_auth
+# @has_access
+# def from_db():
+#     print("some data from database")
+#
+# from_db()
+#
+# def has_access(func):
+#     def wrapper():
+#         if username in USERS:
+#             print("Авторизован как", username)
+#             func()
+#         else:
+#             print("Доступ пользователю", username, "запрещён")
+#     return wrapper
